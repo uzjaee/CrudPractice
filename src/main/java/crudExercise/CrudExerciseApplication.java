a04a5358ea8fef3bf98006d28221844bbcfd6664
uzjaee <kyj960802@gmail.com>
2024-09-20 22:30:57 +0900

    first commit

---
+++ src/main/java/crudExercise/CrudExerciseApplication.java
@@ -0,0 +1,13 @@
+package crudExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExerciseApplication.class, args);
+	}
+
+}
